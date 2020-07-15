8b37da6a02985c79679eeafc80c58075c3de67ec
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2020-07-15 10:34:17 -0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-
-import 'revendaData.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,10 +20,11 @@ class MyApp extends StatelessWidget {
 }
 
 class RevendaModel {
+  final Color color;
   final String marca;
   final String nome;
   final Map<String, String> caracteristicas;
-  RevendaModel({this.marca, this.nome, this.caracteristicas});
+  RevendaModel({this.color, this.marca, this.nome, this.caracteristicas});
 }
 
 class RevendaPage extends StatelessWidget {
@@ -51,7 +48,9 @@ class RevendaPage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           RevendaHeader(),
-          RevendaBody(),
+          Expanded(
+            child: RevendaBody(),
+          ),
         ],
       ),
     );
@@ -73,8 +72,7 @@ class RevendaHeader extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text('Butijões de 13kg em', style: TextStyle(color: Colors.grey)),
-              Text('Av. Paulista, 1001',
-                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('Av. Paulista, 1001', style: TextStyle(fontWeight: FontWeight.bold)),
               Text(
                 'Paulista, São Paulo, SP',
                 style: TextStyle(color: Colors.grey),
@@ -95,24 +93,13 @@ class RevendaHeader extends StatelessWidget {
   }
 }
 
+/*
 class RevendaBody extends StatelessWidget {
   final revendas = [
-    RevendaModel(
-        marca: 'Multimarcas',
-        nome: 'Unigas 1',
-        caracteristicas: {'nota': '4.5', 'tempo': '30-45', 'preco': '74,90'}),
-    RevendaModel(
-        marca: 'Liquigas',
-        nome: 'Yama 2',
-        caracteristicas: {'nota': '4.6', 'tempo': '30-45', 'preco': '72,00'}),
-    RevendaModel(
-        marca: 'Multimarcas',
-        nome: 'Unigas 3',
-        caracteristicas: {'nota': '4.8', 'tempo': '30-50', 'preco': '80,00'}),
-    RevendaModel(
-        marca: 'Multimarcas',
-        nome: 'Mundial Gas 4',
-        caracteristicas: {'nota': '4.7', 'tempo': '30-45', 'preco': '82,90'}),
+    RevendaModel(marca: 'Multimarcas', nome: 'Unigas 1', caracteristicas: {'nota': '4.5', 'tempo': '30-45', 'preco': '74,90'}, color: Colors.black),
+    RevendaModel(marca: 'Liquigas', nome: 'Yama 2', caracteristicas: {'nota': '4.6', 'tempo': '30-45', 'preco': '72,00'}, color: Colors.teal),
+    RevendaModel(marca: 'Multimarcas', nome: 'Unigas 3', caracteristicas: {'nota': '4.8', 'tempo': '30-50', 'preco': '80,00'}, color: Colors.blue),
+    RevendaModel(marca: 'Multimarcas', nome: 'Mundial Gas 4', caracteristicas: {'nota': '4.7', 'tempo': '30-45', 'preco': '82,90'}, color: Colors.black),
   ];
 
   @override
@@ -128,8 +115,38 @@ class RevendaBody extends StatelessWidget {
               nota: revenda.caracteristicas['nota'],
               tempo: revenda.caracteristicas['tempo'],
               preco: revenda.caracteristicas['preco'],
+              color: revenda.color,
             ),
         ],
+      ),
+    );
+  }
+}
+*/
+
+class RevendaBody extends StatelessWidget {
+  final revendas = [
+    RevendaModel(marca: 'Multimarcas', nome: 'Unigas 1', caracteristicas: {'nota': '4.5', 'tempo': '30-45', 'preco': '74,90'}, color: Colors.black),
+    RevendaModel(marca: 'Liquigas', nome: 'Yama 2', caracteristicas: {'nota': '4.6', 'tempo': '30-45', 'preco': '72,00'}, color: Colors.teal),
+    RevendaModel(marca: 'Multimarcas', nome: 'Unigas 3', caracteristicas: {'nota': '4.8', 'tempo': '30-50', 'preco': '80,00'}, color: Colors.blue),
+    RevendaModel(marca: 'Multimarcas', nome: 'Mundial Gas 4', caracteristicas: {'nota': '4.7', 'tempo': '30-45', 'preco': '82,90'}, color: Colors.black),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: revendas.length,
+        itemBuilder: (_, index) {
+          return RevendaCard(
+            marca: revendas[index].marca,
+            nome: revendas[index].nome,
+            nota: revendas[index].caracteristicas['nota'],
+            tempo: revendas[index].caracteristicas['tempo'],
+            preco: revendas[index].caracteristicas['preco'],
+            color: revendas[index].color,
+          );
+        },
       ),
     );
   }
@@ -141,7 +158,8 @@ class RevendaCard extends StatelessWidget {
   final String nota;
   final String tempo;
   final String preco;
-  RevendaCard({this.marca, this.nome, this.nota, this.tempo, this.preco});
+  final Color color;
+  RevendaCard({this.marca, this.nome, this.nota, this.tempo, this.preco, this.color});
 
   @override
   Widget build(BuildContext context) {
@@ -156,10 +174,21 @@ class RevendaCard extends StatelessWidget {
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                //child: RotatedBox(quarterTurns: -1, child: Text('Multimarcas'),),
-                child: RotatedBox(
-                  quarterTurns: -1,
-                  child: Text(marca),
+                child: Container(
+                  width: 30,
+                  height: 100,
+                  color: color,
+                  child: RotatedBox(
+                    quarterTurns: -1,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Text(
+                        marca,
+                        style: TextStyle(color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
                 ),
               ),
               Expanded(
@@ -167,7 +196,6 @@ class RevendaCard extends StatelessWidget {
                   children: <Widget>[
                     Row(
                       children: <Widget>[
-                        //Expanded(child: Text('Unigas')),
                         Expanded(child: Text(nome)),
                         Container(
                           height: 25,
@@ -175,9 +203,7 @@ class RevendaCard extends StatelessWidget {
                           child: Row(
                             children: <Widget>[
                               Icon(Icons.label, color: Colors.white, size: 16),
-                              Text('Melhor Preço',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 14)),
+                              Text('Melhor Preço', style: TextStyle(color: Colors.white, fontSize: 14)),
                             ],
                           ),
                         )
@@ -191,10 +217,7 @@ class RevendaCard extends StatelessWidget {
                             Text('Nota', style: TextStyle(color: Colors.grey)),
                             Row(
                               children: <Widget>[
-                                //Text('4.5', style: TextStyle(fontWeight: FontWeight.bold)),
-                                Text(nota,
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold)),
+                                Text(nota, style: TextStyle(fontWeight: FontWeight.bold)),
                                 Icon(
                                   Icons.star,
                                   color: Colors.yellow,
@@ -205,19 +228,14 @@ class RevendaCard extends StatelessWidget {
                         ),
                         Column(
                           children: <Widget>[
-                            Text('Tempo Medio',
-                                style: TextStyle(color: Colors.grey)),
-                            //Text('30-45 min', style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(tempo,
-                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text('Tempo Medio', style: TextStyle(color: Colors.grey)),
+                            Text(tempo, style: TextStyle(fontWeight: FontWeight.bold)),
                           ],
                         ),
                         Column(
                           children: <Widget>[
                             Text('Preço', style: TextStyle(color: Colors.grey)),
-                            //Text('74,90', style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(preco,
-                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text(preco, style: TextStyle(fontWeight: FontWeight.bold)),
                           ],
                         ),
                       ],
--- lib/main_Last.dart
+++ lib/main_Last.dart
@@ -19,40 +19,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-List<Map> revendasList = [
-  {
-    'marca': 'Multimarcas',
-    'nome': 'Unigas',
-    'caracteristicas': {
-      'nota': '4.5',
-      'tempo medio': '30-45',
-      'preco': '74,90'
-    },},
-  {
-    'marca': 'Liquigas',
-    'nome': 'Yama Gas Bom Pastor',
-    'caracteristicas': {
-      'nota': '4.6',
-      'tempo medio': '30-45',
-      'preco': '72,00'
-    },},
-  {
-    'marca': 'Multimarcas',
-    'nome': 'Unigas',
-    'caracteristicas': {
-      'nota': '4.8',
-      'tempo medio': '30-50',
-      'preco': '80,00'
-    },},
-  {
-    'marca': 'Multimarcas',
-    'nome': 'Mundial Gas',
-    'caracteristicas': {
-      'nota': '4.7',
-      'tempo medio': '30-45',
-      'preco': '82,90'
-    },},
-];
+class RevendaModel {
+  final Color color;
+  final String marca;
+  final String nome;
+  final Map<String, String> caracteristicas;
+  RevendaModel({this.color, this.marca, this.nome, this.caracteristicas});
+}
 
 class RevendaPage extends StatelessWidget {
   @override
@@ -72,31 +45,17 @@ class RevendaPage extends StatelessWidget {
           ),
         ],
       ),
-      body:  Column(
+      body: Column(
         children: <Widget>[
-          Header(),
-          //revendasList.map((e) => null).toList();
-          /*
-          Column(
-            children: <Widget>[
-              SizedBox(height: 20),
-              Card(),
-              SizedBox(height: 20),
-              Card(),
-              SizedBox(height: 20),
-              Card(),
-              SizedBox(height: 20),
-              Card(),
-            ],
-          ),
-          */
+          RevendaHeader(),
+          Expanded(child: SingleChildScrollView(child: RevendaBody())),
         ],
       ),
     );
   }
 }
 
-class Header extends StatelessWidget {
+class RevendaHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -112,88 +71,150 @@ class Header extends StatelessWidget {
             children: <Widget>[
               Text('Butijões de 13kg em', style: TextStyle(color: Colors.grey)),
               Text('Av. Paulista, 1001', style: TextStyle(fontWeight: FontWeight.bold)),
-              Text('Paulista, São Paulo, SP',
-                  style: TextStyle(color: Colors.grey),
+              Text(
+                'Paulista, São Paulo, SP',
+                style: TextStyle(color: Colors.grey),
               ),
             ],
           ),
-         Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: <Widget>[
-           Icon(Icons.location_on, color: Colors.blue),
-           SizedBox(height: 5),
-           Text('MUDAR', style: TextStyle(color: Colors.blue)),
-         ],)
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.location_on, color: Colors.blue),
+              SizedBox(height: 5),
+              Text('MUDAR', style: TextStyle(color: Colors.blue)),
+            ],
+          ),
         ],
       ),
     );
   }
 }
 
-class Card extends StatelessWidget {
+class RevendaBody extends StatelessWidget {
+  final revendas = [
+    RevendaModel(marca: 'Multimarcas', nome: 'Unigas 1', caracteristicas: {'nota': '4.5', 'tempo': '30-45', 'preco': '74,90'}, color: Colors.black),
+    RevendaModel(marca: 'Liquigas', nome: 'Yama 2', caracteristicas: {'nota': '4.6', 'tempo': '30-45', 'preco': '72,00'}, color: Colors.teal),
+    RevendaModel(marca: 'Multimarcas', nome: 'Unigas 3', caracteristicas: {'nota': '4.8', 'tempo': '30-50', 'preco': '80,00'}, color: Colors.blue),
+    RevendaModel(marca: 'Multimarcas', nome: 'Mundial Gas 4', caracteristicas: {'nota': '4.7', 'tempo': '30-45', 'preco': '82,90'}, color: Colors.black),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 20),
-      color: Colors.white,
-      child: Container(
-        child: Row(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RotatedBox(quarterTurns: -1, child: Text('Multimarcas'),),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          for (final revenda in revendas)
+            RevendaCard(
+              marca: revenda.marca,
+              nome: revenda.nome,
+              nota: revenda.caracteristicas['nota'],
+              tempo: revenda.caracteristicas['tempo'],
+              preco: revenda.caracteristicas['preco'],
+              color: revenda.color,
             ),
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Expanded(child: Text('Unigas')),
-                      Container(
-                        height: 25,
-                        color: Colors.orangeAccent,
-                        child: Row(
-                          children: <Widget>[
-                          Icon(Icons.label, color: Colors.white, size: 16),
-                          Text('Melhor Preço', style: TextStyle(color: Colors.white, fontSize: 14)),
-                        ],),
-                      )
-                    ],),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Text('Nota', style: TextStyle(color: Colors.grey)),
-                          Row(
+        ],
+      ),
+    );
+  }
+}
+
+class RevendaCard extends StatelessWidget {
+  final String marca;
+  final String nome;
+  final String nota;
+  final String tempo;
+  final String preco;
+  final Color color;
+  RevendaCard({this.marca, this.nome, this.nota, this.tempo, this.preco, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 10),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          color: Colors.white,
+          child: Container(
+              child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 30,
+                  height: 100,
+                  color: color,
+                  child: RotatedBox(
+                    quarterTurns: -1,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Text(
+                        marca,
+                        style: TextStyle(color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(child: Text(nome)),
+                        Container(
+                          height: 25,
+                          color: Colors.orangeAccent,
+                          child: Row(
                             children: <Widget>[
-                              Text('4.5', style: TextStyle(fontWeight: FontWeight.bold)),
-                              Icon(Icons.star, color: Colors.yellow,),
+                              Icon(Icons.label, color: Colors.white, size: 16),
+                              Text('Melhor Preço', style: TextStyle(color: Colors.white, fontSize: 14)),
                             ],
                           ),
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Text('Tempo Medio', style: TextStyle(color: Colors.grey)),
-                          Text('30-45 min', style: TextStyle(fontWeight: FontWeight.bold)),
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Text('Preço', style: TextStyle(color: Colors.grey)),
-                          Text('74,90', style: TextStyle(fontWeight: FontWeight.bold)),
-                        ],
-                      ),
-                    ],
-                  )
-                ],
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text('Nota', style: TextStyle(color: Colors.grey)),
+                            Row(
+                              children: <Widget>[
+                                Text(nota, style: TextStyle(fontWeight: FontWeight.bold)),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.yellow,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text('Tempo Medio', style: TextStyle(color: Colors.grey)),
+                            Text(tempo, style: TextStyle(fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text('Preço', style: TextStyle(color: Colors.grey)),
+                            Text(preco, style: TextStyle(fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
-        )
-      ),
+            ],
+          )),
+        ),
+      ],
     );
   }
 }
